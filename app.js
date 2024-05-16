4e1364a9b09680a87e315bc4117cf95bfa48f399
Diandra2607 <pradnya.diandra@gmail.com>
2024-05-16 10:33:36 +0700

    new

--- app.js
+++ app.js
@@ -1,115 +1,124 @@
 const todos = [
     {
-        id :1,
-        text : 'Belajar JavaScript',
-        isComplete : false
+      id: 1,
+      text: "Belajar Dasar HTML ",
+      isComplete: true,
     },
     {
-        id :2,
-        text : 'Belajar Javascript 2',
-        isComplete : true
-    },{
-    id :3,
-    text : 'Belajar HTML',
-    isComplete : true
-},
-{
-    id :4,
-    text : 'Belajar CSS',
-    isComplete : false
-}
-];
-
-const generateID = () => {
-    const generatedID = Number(new Date());
-    return generatedID;
-}
-
-window.addEventListener('DOMContentLoaded', () => {
-    const completedTodo = document.querySelector('.completed-todo-list');
-    const uncompletedTodo = document.querySelector('.uncompleted-todo-list');
-    completedTodo.innerHTML = '';
-    uncompletedTodo.innerHTML = '';
-
-    todos.forEach(todo=>{
-        const todoEl = document.createElement('li');
-        const todoText = document.createElement('p');
-        todoText.innerText = todo.text;
-        const actionButtonContainer = document.createElement('div');
-        actionButtonContainer.setAttribute("class","todo-button-container");
-        const doneButton = document.createElement('button');
-        doneButton.setAttribute("class","btn btn-success");
-        doneButton.innerText = 'Done';
-        doneButton.addEventListener('click',()=>{
-            const data = todos.find(td=>todo.id === td.id);
-            console.log(data);
-            if(data) {
-                data.isComplete =true;
-                renderTodoList();
-            }
-        }); 
-        console.log(doneButton);
-
-
-        const removeButton = document.createElement('button');
-        removeButton.setAttribute("class","btn btn-danger");
-        removeButton.innerText = 'Remove';
-        removeButton.addEventListener('click',()=>{
-            const dataIndex = todos.find(td=>todo.id === td.id);
-            console.log(dataIndex);
-            if(data) {
-                todos.splice(dataIndex,1);
-                renderTodoList();
-            }
-        }); 
-
-        const undoButton = document.createElement('button');
-       undoButton.setAttribute("class","btn btn-info");
-        undoButton.innerText = 'Undo';
-        undoButton.addEventListener('click',()=>{
-            const data = todos.find(td=>todo.id === td.id);
-            console.log(data);
-            if(dataIndex) {
-                dataIndex.isComplete =false;
-                renderTodoList();
-            }
-        }); 
-
-        actionButtonContainer.append(todo.isComplete? undoButton:doneButton,removeButton);
-        console.log(actionButtonContainer);
-        todoEl.append(todoText,actionButtonContainer);
-        if(todo.isComplete){
-            completedTodo.append(todoEl);
-        } else{
-            uncompletedTodo.append(todoEl);
-        }
+      id: 2,
+      text: "Belajar Advance HTML",
+      isComplete: false,
+    },
+    {
+      id: 3,
+      text: "Belajar Git",
+      isComplete: false,
+    },
+    {
+      id: 4,
+      text: "Belajar Basic Javascript",
+      isComplete: false,
+    },
+  ];
+  
+  const generateId = () => {
+    const generatedId = +new Date();
+    return generatedId;
+  };
+  
+  const handleDoneButton = (id) => {
+    const todo = todos.find((todo) => todo.id === id);
+    if (todo) {
+      todo.isComplete = true;
+      renderTodoList();
+    }
+  };
+  
+  const handleUndoButton = (id) => {
+    const todo = todos.find((todo) => todo.id === id);
+    if (todo) {
+      todo.isComplete = false;
+      renderTodoList();
+    }
+  };
+  
+  const handleRemoveButton = (id) => {
+    const todoIndex = todos.findIndex((todo) => todo.id === id);
+    if (todoIndex !== undefined) {
+      todos.splice(todoIndex, 1);
+      renderTodoList();
+    }
+  };
+  
+  const createButtonEl = (text, className) => {
+    const buttonEl = document.createElement("button");
+    buttonEl.innerText = text;
+    buttonEl.setAttribute("class", className);
+    return buttonEl;
+  };
+  
+  const createTodoEL = (todo) => {
+    const todoEl = document.createElement("li");
+    const todoText = document.createElement("p");
+    todoText.innerText = todo.text;
+  
+    const actionButtonContainer = document.createElement("div");
+    actionButtonContainer.setAttribute("class", "todo-button-container");
+  
+    const doneButton = createButtonEl("Done", "btn btn-success");
+    doneButton.addEventListener("click", () => {
+      handleDoneButton(todo.id);
     });
-}
-   
-);
-
-const renderTodoList = () => {
-    todos.forEach(todo => {
-
+  
+    const removeButton = createButtonEl("Remove", "btn btn-danger");
+    removeButton.addEventListener("click", () => {
+      handleRemoveButton(todo.id);
     });
-};
-
-window.addEventListener("DOMContentLoaded", () =>{
-    renderTodoList();
-});
-
-
-const form = document.getElementById("todo-form");
-form.addEventListener("submit",(event) =>{
-    event.preventDefault();
-    const todoInput = document.getElementById("todo-input");
-    const newTodo = {
-        id : generateID(),
-        text : todoInput.value,
-        isComplete : false
+  
+    const undoButton = createButtonEl("Undo", "btn btn-info");
+    undoButton.addEventListener("click", () => {
+      handleUndoButton(todo.id);
+    });
+  
+    actionButtonContainer.append(todo.isComplete ? undoButton : doneButton, removeButton);
+    todoEl.append(todoText, actionButtonContainer);
+    return todoEl;
+  };
+  
+  const renderTodoList = () => {
+    const completedTodoList = document.querySelector(".completed-todo-list");
+    const uncompletedTodoList = document.querySelector(".uncompleted-todo-list");
+    completedTodoList.innerHTML = "";
+    uncompletedTodoList.innerHTML = "";
+  
+    todos.forEach((todo) => {
+      const todoEl = createTodoEL(todo);
+      if (!todo.isComplete) uncompletedTodoList.append(todoEl);
+      else completedTodoList.append(todoEl);
+    });
+  };
+  
+  const createPayload = (text) => {
+    return {
+      id: generateId(),
+      text,
+      isComplete: false,
     };
-    todoInput.value = '';
-    todos.push(newTodo);
+  };
+  
+  const handleSubmitForm = () => {
+    const form = document.getElementById("todo-form");
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      const input = document.getElementById("todo-input");
+      const payload = createPayload(input.value);
+      input.value = "";
+      todos.push(payload);
+      renderTodoList();
+    });
+  };
+  
+  window.addEventListener("DOMContentLoaded", () => {
     renderTodoList();
-    
-});
+    handleSubmitForm();
+  });
